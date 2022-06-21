562522e1e58e544800d636baaa661f39d8641417
MateusMoraes05 <playerbeast154@gmail.com>
2022-06-21 08:20:22 -0300

    changes on syntax and added search by animal type

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 import express from "express"
 import mysql from "mysql"
- 
+
 const app = express()
- 
+
 app.use(express.json())
 
- 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
+    host: 'localhost',
+    user: 'root',
+    password: '',
     database: 'tcc_db'
-  });
+});
 
-app.post("/animal", (req, res)=>{
-    const {raca} = req.body
+app.post("/animal", (req, res) => {
+    const reqData = req.body
+    const { raca } = reqData
     connection.query(
         "SELECT * FROM `animal` WHERE `raca` = ?",
         [raca],
-        (error, result)=>{
-            if(error) {
+        (error, result) => {
+            if (error) {
                 console.log(error)
             }
             res.json(result)
@@ -27,5 +27,19 @@ app.post("/animal", (req, res)=>{
     )
 })
 
+app.post("/raca", (req, res) => {
+    const reqData = req.body
+    const { tipo } = reqData
+    connection.query(
+        "SELECT * FROM `animal` WHERE `tipo` = ?",
+        [tipo],
+        (error, result) => {
+            if(error) {
+                console.log(error)
+            }
+            res.json(result)
+        }
+    )
+})
 
 app.listen(3000, "10.0.3.247")
